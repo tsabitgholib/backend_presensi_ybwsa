9bcbf215ec5900ff3fdff0dc409e4104bffd7420
Tsabit Gholib <tsabit1502@gmail.com>
2025-07-20 21:56:47 +0700

    up

--- routes/api.php
+++ routes/api.php
@@ -75,6 +75,7 @@ Route::middleware(['auth.jwt'])->group(function() {
     Route::delete('shift-detail/delete/{id}', [ShiftController::class, 'destroyDetail']);
     Route::get('shift-detail/unit-detail/{unit_detail_id}', [ShiftController::class, 'getByUnitDetail']);
     Route::post('shift-detail/add-pegawai-to-shift-detail', [ShiftController::class, 'assignPegawaiToShiftDetail']);
+    Route::get('shift-detail/get-by-id/{id}', [ShiftController::class, 'getShiftDetailById']);
 
     // Presensi
     Route::post('presensi', [PresensiController::class, 'store']);
